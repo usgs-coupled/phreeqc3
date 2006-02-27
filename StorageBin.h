bc1198144870ac7771c7d59f2f1785be93f65a1e
David L Parkhurst <dlpark@usgs.gov>
2006-02-27 21:24:28 +0000

    Adding StorageBin
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqcpp/trunk@798 1feff8c3-07ed-0310-ac33-dd36852eb9cd

---
+++ StorageBin.h
@@ -0,0 +1,64 @@
+#if !defined(STORAGEBIN_H_INCLUDED)
+#define STORAGEBIN_H_INCLUDED
+
+#include "Parser.h"
+#include "Solution.h"
+#include "Exchange.h"
+#include "GasPhase.h"
+#include "KineticsCxx.h"
+#include "PPassemblage.h"
+#include "SSassemblage.h"
+#include "Surface.h"
+
+#include "Mix.h"
+#include "Reaction.h"
+#include "Temperature.h"
+
+#include <cassert> // assert
+#include <map>     // std::map
+#include <string>  // std::string
+#include <list>    // std::list
+#include <vector>  // std::vector
+
+template<class T> 
+bool exists (std::map<int, T> b, int i){
+	return (b.find(i) != b.end());}
+
+class cxxStorageBin 
+{
+
+public:
+        cxxStorageBin();
+        ~cxxStorageBin();
+
+        //void dump_xml(std::ostream& os, unsigned int indent = 0)const;
+
+        void dump_raw(std::ostream& s_oss, unsigned int indent)const;
+
+        void read_raw(CParser& parser);
+
+protected:
+	// Tidied classes
+	std::map<int, cxxSolution>      Solutions;
+	std::map<int, cxxExchange>      Exchangers;
+	std::map<int, cxxGasPhase>      GasPhases;
+	std::map<int, cxxKinetics>      Kinetics;
+	std::map<int, cxxPPassemblage>  PPassemblages;
+	std::map<int, cxxSSassemblage>  SSassemblages;
+	std::map<int, cxxSurface>       Surfaces;
+
+	//bool b = exists (Solutions, 5);
+	// Initial classes
+	//std::map<int, cxxISolution>     ISolutions;
+
+	// Reaction classes
+	std::map<int, cxxMix>           Mixes;
+	std::map<int, cxxReaction>      Reactions;
+	std::map<int, cxxTemperature>   Temperatures;
+
+public:
+        //static std::map<int, cxxStorageBin>& map;
+
+};
+
+#endif // !defined(STORAGEBIN_H_INCLUDED)
