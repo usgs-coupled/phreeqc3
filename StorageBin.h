7f6f808e85c18dca7108bd2d649694e5a5cdb4cc
David L Parkhurst <dlpark@usgs.gov>
2006-04-17 23:42:03 +0000

    Almost working on unsaturated zone serial version.
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqcpp/trunk/src@867 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- StorageBin.h
+++ StorageBin.h
@@ -34,14 +34,73 @@ public:
 
 	void phreeqc2cxxStorageBin(int n);
 
-	struct cxxSolution *get_solution(int n_user) { 
+	struct cxxSolution *getSolution(int n_user) { 
 		if (this->Solutions.find(n_user) != this->Solutions.end()) {
 			return(&(this->Solutions.find(n_user)->second));
 		} 
 		return (NULL);
 	}
-	void set_solution(int n_user, cxxSolution soln) { 
-		Solutions[n_user] = soln;
+	void setSolution(int n_user, cxxSolution *entity) { 
+		Solutions[n_user] = *entity;
+	}
+
+	struct cxxExchange *getExchange(int n_user) { 
+		if (this->Exchangers.find(n_user) != this->Exchangers.end()) {
+			return(&(this->Exchangers.find(n_user)->second));
+		} 
+		return (NULL);
+	}
+	void setExchange(int n_user, cxxExchange *entity) { 
+		Exchangers[n_user] = *entity;
+	}
+
+	struct cxxPPassemblage *getPPassemblage(int n_user) { 
+		if (this->PPassemblages.find(n_user) != this->PPassemblages.end()) {
+			return(&(this->PPassemblages.find(n_user)->second));
+		} 
+		return (NULL);
+	}
+	void setPPassemblage(int n_user, cxxPPassemblage *entity) { 
+		PPassemblages[n_user] = *entity;
+	}
+	struct cxxGasPhase *getGasPhase(int n_user) { 
+		if (this->GasPhases.find(n_user) != this->GasPhases.end()) {
+			return(&(this->GasPhases.find(n_user)->second));
+		} 
+		return (NULL);
+	}
+	void setGasPhase(int n_user, cxxGasPhase *entity) { 
+		GasPhases[n_user] = *entity;
+	}
+
+	struct cxxSSassemblage *getSSassemblage(int n_user) { 
+		if (this->SSassemblages.find(n_user) != this->SSassemblages.end()) {
+			return(&(this->SSassemblages.find(n_user)->second));
+		} 
+		return (NULL);
+	}
+	void setSSassemblage(int n_user, cxxSSassemblage *entity) { 
+		SSassemblages[n_user] = *entity;
+	}
+
+	struct cxxKinetics *getKinetics(int n_user) { 
+		if (this->Kinetics.find(n_user) != this->Kinetics.end()) {
+			return(&(this->Kinetics.find(n_user)->second));
+		} 
+		return (NULL);
+	}
+	void setKinetics(int n_user, cxxKinetics *entity) { 
+		Kinetics[n_user] = *entity;
+	}
+
+	struct cxxSurface *getSurface(int n_user) { 
+		if (this->Surfaces.find(n_user) != this->Surfaces.end()) {
+			return(&(this->Surfaces.find(n_user)->second));
+		} 
+		return (NULL);
+	}
+	void setSurface(int n_user, cxxSurface *entity) { 
+		Surfaces[n_user] = *entity;
 	}
 
         void dump_raw(std::ostream& s_oss, unsigned int indent)const;
