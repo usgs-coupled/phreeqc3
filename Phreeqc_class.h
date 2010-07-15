778c7b05bc8682f61d41560a37876ed86de999b1
Scott R Charlton <charlton@usgs.gov>
2010-07-15 23:27:37 +0000

    changed p_instance to p_instance1  hiding variables
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqcpp/trunk@4642 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- Phreeqc_class.h
+++ Phreeqc_class.h
@@ -10,11 +10,11 @@
 #define PHREEQC_THIS_COMMA
 #define PHREEQC_NAME_SPACE ::
 #else
-#define P_INSTANCE p_instance
-#define P_INSTANCE_COMMA p_instance,
-#define P_INSTANCE_POINTER p_instance->
-#define PHREEQC_PTR_ARG Phreeqc *p_instance
-#define PHREEQC_PTR_ARG_COMMA Phreeqc *p_instance,
+#define P_INSTANCE p_instance1
+#define P_INSTANCE_COMMA p_instance1,
+#define P_INSTANCE_POINTER p_instance1->
+#define PHREEQC_PTR_ARG Phreeqc *p_instance1
+#define PHREEQC_PTR_ARG_COMMA Phreeqc *p_instance1,
 #define PHREEQC_THIS this
 #define PHREEQC_THIS_COMMA this,
 #define PHREEQC_NAME_SPACE Phreeqc::
