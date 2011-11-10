7271b9d5cbac19c1da1c4b2718f4a5c364cc1e2f
David L Parkhurst <dlpark@usgs.gov>
2011-11-10 15:30:43 +0000

    Replaced TRY/RECOVER with try/catch
    Replaced boolean with bool.
    
    Tony's minor changes to basicsubs and ChartObject.
    
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqcpp/branches/ErrorHandling@5760 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- ChartObject.cpp
+++ ChartObject.cpp
@@ -463,7 +463,7 @@ ChartObject::OpenCSVFile(std::string file_name)
 	if (!f_csv.is_open())
 	{
 		std::ostringstream estream;
-		estream << "Could not open csv file for USER_GRAPH " << file_name;
+		estream << "Could not open csv file for USER_GRAPH " << file_name << "\n       Please, give the full path + filename.";
 		error_msg(estream.str().c_str(), CONTINUE);
 		return false;
 	}
