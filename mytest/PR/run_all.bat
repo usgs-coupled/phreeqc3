d4a5c24430f61184f8d2d14e92de4d28919daaaa
David L Parkhurst <dlpark@usgs.gov>
2011-11-21 18:34:04 +0000

    Tony's gas test cases
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqcpp/branches/ErrorHandling@5794 1feff8c3-07ed-0310-ac33-dd36852eb9cd

---
+++ mytest/PR/run_all.bat
@@ -0,0 +1,15 @@
+call p.bat gas_fix_V.phr
+call p.bat gas_fix_P.phr
+call p.bat PR_mx_new.phr
+call p.bat PR_equil.phr
+call p.bat PR_equil2.phr
+call p.bat PR_EQ.phr
+call p.bat CO2_conc_PR_GP.phr
+call p.bat CO2_conc_PR_IS.phr
+call p.bat CO2_conc_PR_EQ.phr
+call p.bat phi_Duan_PR.phr
+call p.bat P_Vm.phr
+del *.prn
+del gas_8
+del phreeqc.log
+rem del *.out
