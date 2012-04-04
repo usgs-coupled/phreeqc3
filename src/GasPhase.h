53f85d2e3f4207d5911fb7f6a28d9e40a77990ed
Scott R Charlton <charlton@usgs.gov>
2012-04-04 05:04:41 +0000

    const modifications
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqc3/trunk@6358 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- src/GasPhase.h
+++ src/GasPhase.h
@@ -46,11 +46,13 @@ class cxxGasPhase:public cxxNumKeyword
 		return this->totals;
 	};
 	std::vector<cxxGasComp> & Get_gas_comps(void) {return gas_comps;};
+	const std::vector<cxxGasComp> & Get_gas_comps(void)const {return gas_comps;};
 	void Set_gas_comps(const std::vector<cxxGasComp> v) {gas_comps = v;};
 
 	GP_TYPE Get_type(void) const {return type;};
 	void Set_type(GP_TYPE t) {type = t;};
 	LDBLE Get_total_p(void) const {return total_p;};
+	void Set_total_p(LDBLE t) {total_p = t;};
 	LDBLE Get_volume(void) const {return volume;};
 	void Set_volume(LDBLE v) {volume = v;};
 	LDBLE Get_v_m(void) const {return v_m;};
@@ -65,13 +67,11 @@ class cxxGasPhase:public cxxNumKeyword
 	void Set_solution_equilibria(bool tf) {this->solution_equilibria = tf;};
 	int Get_n_solution(void) const {return this->n_solution;};
 	void Set_n_solution(int i) {this->n_solution = i;};
-	LDBLE Get_total_moles(void) {return (LDBLE) total_moles;};
+	LDBLE Get_total_moles(void)const {return total_moles;};
 	void Set_total_moles(LDBLE t) {total_moles = t;};
-	LDBLE Get_total_p(void) {return (LDBLE) total_p;};
-	void Set_total_p(LDBLE t) {total_p = t;};
-	LDBLE Get_temperature(void) {return (LDBLE) temperature;};
+	LDBLE Get_temperature(void)const {return temperature;};
 	void Set_temperature(LDBLE t) {temperature = t;};
-	LDBLE Calc_total_moles(void);
+	LDBLE Calc_total_moles(void)const;
 	cxxGasComp *Find_comp(const char * comp_name);
 
 protected:
