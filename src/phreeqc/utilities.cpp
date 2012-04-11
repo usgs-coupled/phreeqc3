27e4460a6bbb67a93be1bb99cd545ce06d4ad378
Scott R Charlton <charlton@usgs.gov>
2012-04-11 22:48:45 +0000

    changed ifdefs for string_duplicate
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqc3/trunk@6460 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- src/phreeqc/utilities.cpp
+++ src/phreeqc/utilities.cpp
@@ -1129,7 +1129,7 @@ strcmp_nocase_arg1(const char *str1, const char *str2)
 
 /* ---------------------------------------------------------------------- */
 char * Phreeqc::
-#if _DEBUG
+#if !defined(NDEBUG) && defined(WIN32_MEMORY_DEBUG)
 _string_duplicate(const char *token, const char *szFileName, int nLine)
 #else
 string_duplicate(const char *token)
@@ -1142,7 +1142,7 @@ string_duplicate(const char *token)
 	if (token == NULL)
 		return NULL;
 	l = (int) strlen(token);
-#if _DEBUG
+#if !defined(NDEBUG) && defined(WIN32_MEMORY_DEBUG)
 	str = (char *) _malloc_dbg((size_t) (l + 1) * sizeof(char), _NORMAL_BLOCK, szFileName, nLine);
 #else
 	str = (char *) PHRQ_malloc((size_t) (l + 1) * sizeof(char));
