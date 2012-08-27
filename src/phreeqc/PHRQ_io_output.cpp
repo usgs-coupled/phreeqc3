94b0e039eacd86618a30350432c22c3b74cb39d1
David L Parkhurst <dlpark@usgs.gov>
2012-08-27 14:21:47 +0000

    Tony's changes for dates
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqc3/trunk@6923 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- src/phreeqc/PHRQ_io_output.cpp
+++ src/phreeqc/PHRQ_io_output.cpp
@@ -321,7 +321,7 @@ process_file_names(int argc, char *argv[], std::istream **db_cookie,
 		screen_msg(sformatf("Database file: %s\n\n", token));
 		strcpy(db_file, token);
 #ifdef NPP
-		output_msg(sformatf("Using PHREEQC: version 3.beta, compiled on May 22, 2012\n"));
+		output_msg(sformatf("Using PHREEQC: version 3.beta, compiled on August 6, 2012\n"));
 #endif
 		output_msg(sformatf("   Input file: %s\n", in_file));
 		output_msg(sformatf("  Output file: %s\n", out_file));
