6852b695310d9898df6e0ede7f4799946e30234e
David L Parkhurst <dlpark@usgs.gov>
2012-12-19 01:01:19 +0000

    Removed input files
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqc3/trunk@7233 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- mytest/mytest_pc/Run_other.bat
+++
@@ -1,7 +0,0 @@
-..\ClrClass_release\phreeqcpp.exe densities
-..\ClrClass_release\phreeqcpp.exe diel
-..\ClrClass_release\phreeqcpp.exe H_K
-..\ClrClass_release\phreeqcpp.exe Sat_P_rho
-#..\ClrClass_release\phreeqcpp.exe CaKMgNaCl
-..\ClrClass_release\phreeqcpp.exe rho25
- 
