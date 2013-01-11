6181464978887ae2acad7e50bdf5a895e9e68a66
Scott R Charlton <charlton@usgs.gov>
2013-01-11 02:17:56 +0000

    added tests for make check
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqc3/branches/reorg@7314 1feff8c3-07ed-0310-ac33-dd36852eb9cd

---
+++ test/ex15a.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+# Test ex15a
+
+NAME=ex15a
+DB=ex15.dat
+
+: ${PHREEQC=phreeqc}
+${PHREEQC} $DATADIR/$NAME $NAME.out $DATADIR/$DB $NAME.log
+result=$?
+
+exit $result
---
+++ test/ex5.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+# Test ex5
+
+NAME=ex5
+DB=phreeqc.dat
+
+: ${PHREEQC=phreeqc}
+${PHREEQC} $DATADIR/$NAME $NAME.out $DBDIR/$DB $NAME.log
+result=$?
+
+exit $result
