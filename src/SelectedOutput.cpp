25e18731eece25be96b4c04c23b498ae67bc4f42
Scott R Charlton <charlton@usgs.gov>
2013-10-01 05:34:06 +0000

    rearranged vars and added code to track changes
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqc3/trunk@8093 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- src/SelectedOutput.cpp
+++ src/SelectedOutput.cpp
@@ -3,44 +3,66 @@
 SelectedOutput::SelectedOutput(int n, PHRQ_io *io)
 :	cxxNumKeyword(io)
 {
+	// file_name
 	this->Set_file_name(n);
+
+	// punch_ostream
 	this->punch_ostream = NULL;
-	this->active = true;
-	this->new_def = false;
+
+	// state vars
+	this->active             = true;
+	this->new_def            = false;
 	this->user_punch_new_def = false;
-	//std::map<std::string, void *> totals;
-	//std::map<std::string, void *> molalities;
-	//std::map<std::string, void *> activities;
-	//std::map<std::string, void *> pure_phases;
-	//std::map<std::string, void *> si;
-	//std::map<std::string, void *> gases;
-	//std::map<std::string, void *> s_s;
-	//std::map<std::string, void *> kinetics;
-	//std::map<std::string, void *> isotopes;
-	//std::map<std::string, void *> calculate_values;
-	//std::map<std::string, void *> kinetics;
-	//std::map<std::string, void *> kinetics;
+	this->have_punch_name    = false;
+
+	// as-is vars
+	//
+	this->user_punch       = true;
+	this->high_precision   = false;
 	this->inverse          = true;
+
 	this->sim              = true;
 	this->state            = true;
 	this->soln             = true;
 	this->dist             = true;
 	this->time             = true;
+
 	this->step             = true;
-	this->rxn              = false;
-	this->temp             = false;
 	this->ph               = true;
 	this->pe               = true;
+	this->rxn              = false;
+	this->temp             = false;
+
 	this->alk              = false;
 	this->mu               = false;
 	this->water            = false;
-	this->high_precision   = false;
-	this->user_punch       = true;
 	this->charge_balance   = false;
 	this->percent_error    = false;
-	this->have_punch_name  = false;
-}
 
+	// as-is set flags
+	//
+	this->set_user_punch     = false;
+	this->set_high_precision = false;
+	this->set_inverse        = false;
+
+	this->set_sim            = false;
+	this->set_state          = false;
+	this->set_soln           = false;
+	this->set_dist           = false;
+	this->set_time           = false;
+
+	this->set_step           = false;
+	this->set_ph             = false;
+	this->set_pe             = false;
+	this->set_rxn            = false;
+	this->set_temp           = false;
+
+	this->set_alk            = false;
+	this->set_mu             = false;
+	this->set_water          = false;
+	this->set_charge_balance = false;
+	this->set_percent_error  = false;
+}
 
 SelectedOutput::~SelectedOutput(void)
 {
@@ -50,25 +72,30 @@ SelectedOutput::~SelectedOutput(void)
 	}
 	this->punch_ostream = NULL;
 }
+
 void
 SelectedOutput::Reset(bool value)
 {
-	sim = value;
-	state = value;
-	soln = value;
-	dist = value;
-	time = value;
-	step = value;
-	ph = value;
-	pe = value;
-	rxn = value;
-	temp = value;
-	alk = value;
-	mu = value;
-	water = value;
+	// as-is vars
+	sim            = value;
+	state          = value;
+	soln           = value;
+	dist           = value;
+	time           = value;
+
+	step           = value;
+	ph             = value;
+	pe             = value;
+	rxn            = value;
+	temp           = value;
+
+	alk            = value;
+	mu             = value;
+	water          = value;
 	charge_balance = value;
-	percent_error = value;
+	percent_error  = value;
 }
+
 void
 SelectedOutput::Set_file_name(int n)
 {
