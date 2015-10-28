840ce4f3075c91889f7b635a4eca236edac81096
Scott R Charlton <charlton@usgs.gov>
2015-10-28 01:57:04 +0000

    added jenkins test script
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqc3/trunk@10438 1feff8c3-07ed-0310-ac33-dd36852eb9cd

---
+++ jenkins-phreeqc-tester.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+failed=0
+cd src || exit
+
+make -j 20 -f Makefile.old Class_release_64 CL1MP_LIB=/usr/lib/x86_64-linux-gnu/libgmp.a
+
+cd ../mytest || exit
+
+test(){
+  echo "Testing ${1}"
+  mv "${1}_101.sel" "${1}_101.sel.expected"
+  make "${1}.out" > /dev/null
+  diff "${1}_101.sel" "${1}_101.sel.expected"  > "${1}.diff"
+  if [ "$?" != "0" ]; then
+    echo "  FAILED"
+    head "${1}.diff"
+    failed=1
+  fi
+}
+
+#{{
+sed -i -e 's/8.0/7.0/g' alkalinity
+#}}
+
+for f in *_101.sel; do
+  f=`echo "${f}" | sed 's/_101\.sel$//'`
+  test "${f}"
+done
+
+if [ ${failed} != 0 ]; then
+  false
+fi
