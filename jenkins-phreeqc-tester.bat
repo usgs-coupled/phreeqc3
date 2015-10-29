4c15cc7842fca4b5bfd978388571cb0b84fa18e7
Scott R Charlton <charlton@usgs.gov>
2015-10-29 23:31:48 +0000

    removed alkalinity fail; added filesize test
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqc3/trunk@10461 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- jenkins-phreeqc-tester.bat
+++ jenkins-phreeqc-tester.bat
@@ -7,19 +7,13 @@ set PATH=%PATH%;%PATH_DIFF%
 
 cd mytest
 
-sed -i -e "s/8.0/7.0/g" alkalinity
-
-REM move alkalinity_101.sel alkalinity_101.sel.expected
-REM ..\x64\Release\phreeqc alkalinity alkalinity.out xxx alkalinity.log
-REM diff alkalinity_101.sel alkalinity_101.sel.expected > alkalinity.diff
-
-REM @echo off
+REM Uncomment next line to force reldiff error
+REM sed -i -e "s/8.0/7.0/g" alkalinity
 
 for %%f in (*_101.sel) do (
   set ff=%%f
   call :test !ff:_101.sel=!
 )
-REM echo failed=!failed!
 exit /b !failed!
 goto :EOF
 
@@ -28,6 +22,8 @@ echo Testing %1
 move %1_101.sel %1_101.sel.expected > NUL
 ..\x64\Release\phreeqc %1 %1.out xxx %1.log
 diff %1_101.sel %1_101.sel.expected > %1.diff
+SET FILESIZE=%~z1
+echo %FILESIZE%
 reldiff %1
 if %ERRORLEVEL% NEQ 0 (
   echo "  FAILED"
