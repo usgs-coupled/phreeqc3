a73152829e33fadbda2a7557c13451414f1647cf
David L Parkhurst <dlpark@usgs.gov>
2016-03-03 17:59:50 +0000

    revised fixup_html to work with current windows (quoted one variable) and automatically remove extra headings in phreeqc3.htm.
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqc3/trunk@10882 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- HTMLversion/HTML/fixup_html.bash
+++ HTMLversion/HTML/fixup_html.bash
@@ -50,14 +50,15 @@ echo                     Done UpdateContents
 # Fix example 19 in phreeqc3.htm
 export HEAD19='CLASS="XRef">Modeling Cd+2 Sorption With Linear, Freundlich, and Langmuir Isotherms, and With a Deterministic Distribution of Sorption Sites for Organic Matter, Clay Minerals, and Iron Oxyhydroxides</A></A></A></P>'
 export NAME19=`grep 'Example 19--<A NAME=' phreeqc3-74.htm | sed "s/^.*\([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]\_[0-9][0-9][0-9][0-9][0-9]\)\(.*\)/\1/" `
-export REST='"phreeqc3-74.htm#'$NAME19'" '$HEAD19
+export REST='"phreeqc3-74.htm#'$NAME19'" '"$HEAD19"
 sed "s?\(^.*phreeqc3-74.*19. <A HREF=\)\(.*\)?\1$REST?" phreeqc3.htm > out; mv out phreeqc3.htm
 echo                     Done Example 19
 
 #cd HTML; egrep '<H1 CLASS="FM1stOrderHeadingTOC">' -A1 -v phreeqc3.htm > temp3; mv temp3 phreeqc3.htm
+egrep '<H1' -v phreeqc3.htm | egrep '/H1>' -v > temp3; mv temp3 phreeqc3.htm
 
+#echo
+#echo "(1) Need to mannually delete extra table of contents in phreeqc3.htm"
 echo
-echo "(1) Need to mannually delete extra table of contents in phreeqc3.htm"
-echo
-echo "(2) Compile HTML, double click on phreeqc3.hhp and hit compile button"
+echo "(1) Compile HTML, double click on phreeqc3.hhp and hit compile button"
 
