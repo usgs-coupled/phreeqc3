8d624a7f00222bd172596238bfb24d2d4359569c
Scott R Charlton <charlton@usgs.gov>
2017-02-01 02:25:18 +0000

    fixed compilation for linux c++11
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqc3/trunk@12301 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- kinetics.cpp
+++ kinetics.cpp
@@ -1253,7 +1253,7 @@ set_and_run_wrapper(int i, int use_mix, int use_kinetics, int nsaver,
 	int old_diag, old_itmax;
 	LDBLE old_tol, old_min_value, old_step, old_pe, old_pp_column_scale;
 	LDBLE small_pe_step, small_step;
-#if (__GNUC__ && (__cplusplus >= 201103L) || (_MSC_VER >= 1600)
+#if (__GNUC__ && (__cplusplus >= 201103L)) || (_MSC_VER >= 1600)
 	std::unique_ptr<cxxPPassemblage> pp_assemblage_save=NULL;
 	std::unique_ptr<cxxSSassemblage> ss_assemblage_save=NULL;
 	std::unique_ptr<cxxKinetics> kinetics_save=NULL;
