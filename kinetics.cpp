80f143a6c6404d0311727f8c47f34220342ca4f4
Scott R Charlton <charlton@usgs.gov>
2017-02-01 00:37:57 +0000

    changed macros for unique_ptr
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqc3/trunk@12294 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- kinetics.cpp
+++ kinetics.cpp
@@ -1253,14 +1253,14 @@ set_and_run_wrapper(int i, int use_mix, int use_kinetics, int nsaver,
 	int old_diag, old_itmax;
 	LDBLE old_tol, old_min_value, old_step, old_pe, old_pp_column_scale;
 	LDBLE small_pe_step, small_step;
-#ifdef SOMETHING
-	std::auto_ptr<cxxPPassemblage> pp_assemblage_save(NULL);
-	std::auto_ptr<cxxSSassemblage> ss_assemblage_save(NULL);
-	std::auto_ptr<cxxKinetics> kinetics_save(NULL);
-#else
+#if (__cplusplus >= 201103L) || (_MSC_VER >= 1600)
 	std::unique_ptr<cxxPPassemblage> pp_assemblage_save=NULL;
 	std::unique_ptr<cxxSSassemblage> ss_assemblage_save=NULL;
 	std::unique_ptr<cxxKinetics> kinetics_save=NULL;
+#else
+	std::auto_ptr<cxxPPassemblage> pp_assemblage_save(NULL);
+	std::auto_ptr<cxxSSassemblage> ss_assemblage_save(NULL);
+	std::auto_ptr<cxxKinetics> kinetics_save(NULL);
 #endif
 
 	
