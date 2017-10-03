5447a2bb0cc0af842690600e515ab3de6910e0b0
David L Parkhurst <dlpark@usgs.gov>
2017-10-03 20:55:36 +0000

    Making consistent
    phi = (phi > 4.44 ? 4.44 : (phi < -3 ? -3 : phi));
    
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqc3/branches/concrete@12860 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- src/prep.cpp
+++ src/prep.cpp
@@ -4152,6 +4152,8 @@ calc_PR(std::vector<struct phase *> phase_ptrs, LDBLE P, LDBLE TK, LDBLE V_m)
 			phi = B_r * (rz - 1) - log(rz - B) + A / (2.828427 * B) * (B_r - 2.0 * phase_ptr->pr_aa_sum2 / a_aa_sum) *
 				log((rz + 2.41421356 * B) / (rz - 0.41421356 * B));
 			phi = (phi > 4.44 ? 4.44 : (phi < -3 ? -3 : phi));
+			//if (phi > 4.44)
+			//	phi = 4.44;
 		}
 		else
 			phi = -3.0; // fugacity coefficient > 0.05
