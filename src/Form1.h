6c0c4410c31d6d9565e0930191a95bef062bdf9f
Scott R Charlton <charlton@usgs.gov>
2017-10-31 19:55:15 +0000

    fixed to compile in vs2005
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqc3/branches/concrete-2005@12903 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- src/Form1.h
+++ src/Form1.h
@@ -1134,8 +1134,11 @@ namespace zdg_ui2 {
 			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
 			try
 			{
-				//this->Icon = (cli::safe_cast<System::Drawing::Icon^  >(resources->GetObject(L"$this.Icon")));
+#ifdef NPP
 				this->Icon = gcnew System::Drawing::Icon("c:\\phreeqc\\phreex.ico");
+#else
+				this->Icon = (cli::safe_cast<System::Drawing::Icon^  >(resources->GetObject(L"$this.Icon")));
+#endif
 			}
 			catch (...)
 			{
