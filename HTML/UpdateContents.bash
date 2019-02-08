ecb30d2088b5f9d3dd9605ac558455395f3b546e
David Parkhurst <dlpark@usgs.gov>
2019-02-07 21:27:41 -0700

    UpdateContents.bash, contents.hhc, contents.template.hhc, .fm files

--- HTML/UpdateContents.bash
+++ HTML/UpdateContents.bash
@@ -17,27 +17,27 @@ export DESC=`grep 'Description of Data Input</H2>' phreeqc3-5.htm | sed "s/^.*\(
 #	phreeqc3-54.htm
 #	The Basic Interpreter</H2>
 #<A NAME="50515973_pgfId-470977"></A><A NAME="50515973_44206"></A>The Basic Interpreter</H2>
-export BAS=`grep 'The Basic Interpreter</H2>' phreeqc3-54.htm | sed "s/^.*\([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]\_[0-9][0-9][0-9][0-9][0-9]\)/\1/" | sed "s/\([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]\).*/\1/"`
+export BAS=`grep 'The Basic Interpreter</H2>' phreeqc3-61.htm | sed "s/^.*\([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]\_[0-9][0-9][0-9][0-9][0-9]\)/\1/" | sed "s/\([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]\).*/\1/"`
 #echo $BAS
 
 	
 #xxxExamplesxxx
 #	phreeqc3-55.htm
 #	Examples</H2>
-export EXAM=`grep 'Examples</H2>' phreeqc3-55.htm | sed "s/^.*\([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]\_[0-9][0-9][0-9][0-9][0-9]\)/\1/" | sed "s/\([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]\).*/\1/"`
+export EXAM=`grep 'Examples</H2>' phreeqc3-62.htm | sed "s/^.*\([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]\_[0-9][0-9][0-9][0-9][0-9]\)/\1/" | sed "s/\([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]\).*/\1/"`
 #echo $EXAM
 
 
 #xxxReferencesxxx
 #	phreeqc3-78.htm
 #	References Cited</H2>
-export REF=`grep 'References Cited</H2>' phreeqc3-78.htm | sed "s/^.*\([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]\_[0-9][0-9][0-9][0-9][0-9]\)/\1/" | sed "s/\([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]\).*/\1/"`
+export REF=`grep 'References Cited</H2>' phreeqc3-85.htm | sed "s/^.*\([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]\_[0-9][0-9][0-9][0-9][0-9]\)/\1/" | sed "s/\([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]\).*/\1/"`
 #echo $REF
 
 #xxxProgrammersxxx
 #	phreeqc3-79.htm
 #	Keyword Data Blocks for Programmers</H2>
-export PROG=`grep 'Keyword Data Blocks for Programmers</H2>' phreeqc3-79.htm | sed "s/^.*\([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]\_[0-9][0-9][0-9][0-9][0-9]\)/\1/" | sed "s/\([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]\).*/\1/"`
+export PROG=`grep 'Keyword Data Blocks for Programmers</H2>' phreeqc3-86.htm | sed "s/^.*\([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]\_[0-9][0-9][0-9][0-9][0-9]\)/\1/" | sed "s/\([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]\).*/\1/"`
 #echo $PROG
 
 sed -e "s/xxxIntroductionxxx/$INTRO/g" -e "s/xxxDescriptionxxx/$DESC/g" -e "s/xxxBasicxxx/$BAS/g" -e "s/xxxExamplesxxx/$EXAM/g" -e "s/xxxReferencesxxx/$REF/g" -e "s/xxxProgrammersxxx/$PROG/g" ../contents.template.hhc > ../contents.hhc
