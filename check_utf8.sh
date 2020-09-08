6c157761a0d49b94de42c13dabbd3cbfbeb24b16
Charlton, Scott R <charlton@usgs.gov>
2020-09-08 14:15:45 -0700

    fixed check_utf8.sh return value

--- check_utf8.sh
+++ check_utf8.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+EXIT_CODE=0
 files=$(find . -type f -name "*.cpp" -o -name "*.cxx" -o -name "*.h" -o -name "Makefile.am" -o -name "CMakeLists.txt" -o -name "configure.ac" -o -wholename "*/database/*.dat")
 for f in $files
 do
-    iconv -f UTF-8 "$f" > /dev/null || file "$f"
+    if ! iconv -f UTF-8 "$f" > /dev/null; then
+	EXIT_CODE=1
+	file "$f"
+    fi
 done
+exit $EXIT_CODE
