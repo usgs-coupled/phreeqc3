5cb7a4a7a14a80659fbe3df1cd1560625291dd44
Charlton, Scott R <charlton@usgs.gov>
2023-01-05 16:36:46 -0700

    Record commit using ctest_update

--- ctest.cmake
+++ ctest.cmake
@@ -3,7 +3,6 @@ include(CTestConfig.cmake)
 site_name(CTEST_SITE)
 set(CTEST_BUILD_NAME ${CMAKE_HOST_SYSTEM_NAME})
 
-
 set(CTEST_SOURCE_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}")
 set(CTEST_BINARY_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}/_ctest")
 
@@ -11,11 +10,15 @@ set(CTEST_BINARY_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}/_ctest")
 set(CTEST_CMAKE_GENERATOR Ninja)
 set(CTEST_USE_LAUNCHERS 1)
 
+set(CTEST_UPDATE_TYPE git)
+set(CTEST_UPDATE_COMMAND git)
+
 ##set(CTEST_COVERAGE_COMMAND "gcov")
 find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
 
-##ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
 ctest_start("Continuous")
+ctest_update()
 ctest_configure(OPTIONS "-DCMAKE_BUILD_TYPE=Debug")
 ctest_build()
 ctest_test(INCLUDE "mytest.*.ex10$")
