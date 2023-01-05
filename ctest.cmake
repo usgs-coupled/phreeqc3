eb2b2d3177f80711cdb98b14ca2b1aa92f65b16d
Charlton, Scott R <charlton@usgs.gov>
2023-01-04 18:24:23 -0700

    working on github testing

---
+++ ctest.cmake
@@ -0,0 +1,20 @@
+set(CTEST_SOURCE_DIRECTORY ".")
+set(CTEST_BINARY_DIRECTORY "_ctest")
+
+##set(ENV{CXXFLAGS} "--coverage")
+set(CTEST_CMAKE_GENERATOR "Ninja")
+set(CTEST_USE_LAUNCHERS 1)
+
+##set(CTEST_COVERAGE_COMMAND "gcov")
+find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
+
+##ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+ctest_start("Continuous")
+ctest_configure(OPTIONS "-DCMAKE_BUILD_TYPE=Debug")
+ctest_build()
+ctest_test(INCLUDE "mytest.*.ex10$")
+#ctest_coverage()
+if (CTEST_MEMORYCHECK_COMMAND)
+  ctest_memcheck(INCLUDE "mytest.*.ex10$" EXCLUDE "numdiff")
+endif()
+#ctest_submit()
