3e6b8b2c8959b993107e38307b1c48723c9241e6
Charlton, Scott R <charlton@usgs.gov>
2023-01-08 17:13:40 -0700

    Testing numdiff

---
+++ charlton.cmake
@@ -0,0 +1,32 @@
+include(CTestConfig.cmake)
+
+site_name(CTEST_SITE)
+set(CTEST_BUILD_NAME ${CMAKE_HOST_SYSTEM_NAME})
+
+set(CTEST_SOURCE_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}")
+set(CTEST_BINARY_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}/_charlton")
+
+##set(ENV{CXXFLAGS} "--coverage")
+set(CTEST_CMAKE_GENERATOR Ninja)
+set(CTEST_CONFIGURATION_TYPE Debug)
+set(CTEST_BUILD_CONFIGURATION Debug)
+set(CTEST_USE_LAUNCHERS 1)
+
+set(CTEST_UPDATE_TYPE git)
+set(CTEST_UPDATE_COMMAND git)
+set(CTEST_UPDATE_VERSION_ONLY TRUE)
+
+##set(CTEST_COVERAGE_COMMAND "gcov")
+find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
+
+ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+ctest_start("Continuous")
+ctest_update()
+ctest_configure()
+ctest_build()
+ctest_test(INCLUDE "mytest.*.ex10$")
+#ctest_coverage()
+if (CTEST_MEMORYCHECK_COMMAND)
+  ctest_memcheck(INCLUDE "mytest.*.ex10$" EXCLUDE "numdiff")
+endif()
+###ctest_submit()
