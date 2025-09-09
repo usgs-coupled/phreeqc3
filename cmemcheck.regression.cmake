25c0b2a6da679fc6599614daa76da3342a76912b
Charlton, Scott R. <charlton.scott.dev@gmail.com>
2025-09-08 20:54:56 -0600

    Added Regression option to workflow_dispatch

---
+++ cmemcheck.regression.cmake
@@ -0,0 +1,33 @@
+include(CTestConfig.cmake)
+
+site_name(CTEST_SITE)
+set(CTEST_BUILD_NAME ${CMAKE_HOST_SYSTEM_NAME})
+
+set(CTEST_SOURCE_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}")
+set(CTEST_BINARY_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}/_cmemcheck")
+
+##set(ENV{CXXFLAGS} "--coverage")
+set(CTEST_CMAKE_GENERATOR Ninja)
+set(CTEST_CONFIGURATION_TYPE Debug)
+set(CTEST_BUILD_CONFIGURATION Debug)
+set(CTEST_USE_LAUNCHERS 1)
+
+set(CTEST_UPDATE_TYPE git)
+set(CTEST_UPDATE_COMMAND git)
+set(CTEST_UPDATE_VERSION_ONLY TRUE)
+
+##find_program(CTEST_COVERAGE_COMMAND NAMES gcov)
+find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
+
+ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+ctest_start("Continuous")
+ctest_update()
+ctest_configure(OPTIONS "-DPHRQC_TESTING:BOOL=ON;-DPHRQC_ENABLE_REGRESSION_TESTING:BOOL=ON")
+ctest_build()
+if(${CMAKE_VERSION} VERSION_LESS "3.29")
+  ctest_memcheck(EXCLUDE "numdiff" PARALLEL_LEVEL 2)
+else()
+  ctest_memcheck(EXCLUDE "numdiff" PARALLEL_LEVEL)
+endif()
+##ctest_coverage()
+ctest_submit()
